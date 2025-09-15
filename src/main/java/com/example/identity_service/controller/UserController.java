a2380054ccb4a147504d24d227e77c4723850104
VoDongPhu-qn123 <phuuit169@gmail.com>
2025-09-15 10:54:15 +0700

    Initial commit

---
+++ src/main/java/com/example/identity_service/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.identity_service.controller;
+
+import com.example.identity_service.dto.request.UserCreationRequest;
+import com.example.identity_service.dto.request.UserUpdateRequest;
+import com.example.identity_service.entity.User;
+import com.example.identity_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping
+    User createUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+    @GetMapping
+    List<User> getUsers() {
+        return  userService.getUsers();
+    }
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId) {
+        return  userService.getUser(userId);
+    }
+    @PutMapping("{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId,request);
+    }
+    @PatchMapping("{userId}")
+    User updatePatchUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return userService.updatePathchUser(userId,request);
+    }
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId) {
+         userService.deleteUser(userId);
+         return "User has been deleted";
+    }
+}
---
+++ src/main/java/com/example/identity_service/service/UserService.java
@@ -0,0 +1,60 @@
+package com.example.identity_service.service;
+
+import com.example.identity_service.dto.request.UserCreationRequest;
+import com.example.identity_service.dto.request.UserUpdateRequest;
+import com.example.identity_service.entity.User;
+import com.example.identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDayOfBirth(request.getDayOfBirth());
+        return userRepository.save(user);
+    }
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+    public User getUser(String id) {
+        return  userRepository.findById(id).orElseThrow(()->new RuntimeException("User not found"));
+    }
+    public  User updateUser(String id, UserUpdateRequest request) {
+        User user = getUser(id);
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDayOfBirth(request.getDayOfBirth());
+        return  userRepository.save(user);
+    }
+    public  User updatePathchUser(String id, UserUpdateRequest request) {
+        User user = getUser(id);
+        if(request.getPassword() != null) {
+            user.setPassword(request.getPassword());
+        }
+        if(request.getFirstName() != null) {
+            user.setFirstName(request.getFirstName());
+        }
+        if(request.getLastName() != null) {
+            user.setLastName(request.getLastName());
+        }
+        if(request.getDayOfBirth() != null) {
+            user.setDayOfBirth(request.getDayOfBirth());
+        }
+
+        return  userRepository.save(user);
+    }
+    public void deleteUser(String id) {
+        User user = getUser(id);
+        userRepository.deleteById(id);
+    }
+}
